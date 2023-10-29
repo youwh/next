8d6b8546da5d8d7ca75f0e197810e35d1be43239
H! <>
2023-10-29 10:01:42 +0800

    test config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,13 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-  baseUrl: process.env.NODE_ENV === 'production' ? '/next' : '',
+  // baseUrl: process.env.NODE_ENV === 'production' ? '/next' : '',
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.output.publicPath = process.env.NODE_ENV === 'production' ? '/next/' : '/';
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
