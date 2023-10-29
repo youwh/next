9fc6a8034ec15e2018f32a8b6800565d231f8998
H! <>
2023-10-29 10:26:08 +0800

    add more basePath

--- src/components/home/components/InsightCard.tsx
+++ src/components/home/components/InsightCard.tsx
@@ -1,3 +1,4 @@
+import { basePath } from '~/tempfix';
 import { ArrowSvg2 } from '~/ui/svgs/arrow2';
 import style from './insightcard.module.css';
 
@@ -5,7 +6,7 @@ export const InsightCard = ({ data }: any) => {
   return (
     <div className={`${style.card}`}>
       <div className={style.top}>
-        <img className={style.image} src={data.image} />
+        <img className={style.image} src={basePath + data.image} />
         <div className={`${style.tag} ${style['type' + data.type]}`}>{data.category}</div>
       </div>
       <div className={style.content}>
@@ -13,7 +14,7 @@ export const InsightCard = ({ data }: any) => {
         <div>{data.title}</div>
         <div>{data.description}</div>
         <div className={style.author}>
-          <img src={data.author.avatar} alt={`${data.author.name}`} />
+          <img src={basePath + data.author.avatar} alt={`${data.author.name}`} />
           <span>{data.author.name}</span>
         </div>
       </div>
--- src/components/home/components/SectorCard.tsx
+++ src/components/home/components/SectorCard.tsx
@@ -1,9 +1,10 @@
+import { basePath } from '~/tempfix';
 import style from './sectorcard.module.css';
 
 export const SectorCard = ({ data }: any) => {
   return (
     <div className={style.card}>
-      <img className={style.image} src={data.img}></img>
+      <img className={style.image} src={basePath + data.img}></img>
       <div className={style.content}>
         <span className={style.title}>{data.title}</span>
         <a className={`big-button ${style.button}`}>View more</a>
